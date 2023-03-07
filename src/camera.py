1f49b5814a929dfac253de40124d4a83c356797a
dakshinatharindu <tharinduokd@gmail.com>
2023-03-08 00:54:39 +0530

    created Camera class

--- src/camera.py
+++ src/camera.py
@@ -1,34 +1,33 @@
-# import the necessary packages
-from picamera.array import PiRGBArray
-from picamera import PiCamera
-import time
 import cv2
 import numpy as np
 
-DIM=(640, 480)
-K=np.array([[258.834980287596, 0.0, 315.06271814581396], [0.0, 259.2571348729949, 229.82338632554638], [0.0, 0.0, 1.0]])
-D=np.array([[0.03771034946935066], [-0.06873597576438498], [0.19581964746039696], [-0.2649961510264901]])
-map1, map2 = cv2.fisheye.initUndistortRectifyMap(K, D, np.eye(3), K, DIM, cv2.CV_16SC2)
+# fisheye cleaning parameters
+DIM = (640, 480)
+K = np.array([[258.834980287596, 0.0, 315.06271814581396], [0.0, 259.2571348729949, 229.82338632554638], [0.0, 0.0, 1.0]])
+D = np.array([[0.03771034946935066], [-0.06873597576438498], [0.19581964746039696], [-0.2649961510264901]])
+MAP_1, MAP_2 = cv2.fisheye.initUndistortRectifyMap(K, D, np.eye(3), K, DIM, cv2.CV_16SC2)
 
+class Camera:
+	def __init__(self):
+		# private
+		self._cap = cv2.VideoCapture(0)
 
-# initialize the camera and grab a reference to the raw camera capture
-camera = PiCamera()
-camera.resolution = (640, 480)
-camera.framerate = 32
-rawCapture = PiRGBArray(camera, size=(640, 480))
-# allow the camera to warmup
-time.sleep(0.1)
-# capture frames from the camera
-for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=True):
-	# grab the raw NumPy array representing the image, then initialize the timestamp
-	# and occupied/unoccupied text
-	image = frame.array
-	image = cv2.remap(image, map1, map2, interpolation=cv2.INTER_LINEAR, borderMode=cv2.BORDER_CONSTANT)
-	# show the frame
-	cv2.imshow("Frame", image)
-	key = cv2.waitKey(1) & 0xFF
-	# clear the stream in preparation for the next frame
-	rawCapture.truncate(0)
-	# if the `q` key was pressed, break from the loop
-	if key == ord("q"):
-		break
+		# public
+		self.WIDTH = DIM[0]
+		self.HEIGHT = DIM[1]
+	
+	def get_frame(self) -> np.ndarray:
+		ret, frame = self._cap.read()
+		assert ret, "There is an error while reading VideoCapture"
+
+		# undistort fisheye
+		frame = cv2.remap(frame, MAP_1, MAP_2, interpolation=cv2.INTER_LINEAR, borderMode=cv2.BORDER_CONSTANT)
+
+		return frame
+	
+	def __del__(self):
+		self._cap.release()
+
+if __name__ == "__main__":
+    print("Please run the 'main.py' file")
+	
