1a32113f922c0d94bb73ab8b61fe9253bb922528
dakshinatharindu <tharinduokd@gmail.com>
2023-03-13 15:22:19 +0530

    otsu thresholding

--- src/main.py
+++ src/main.py
@@ -1,12 +1,16 @@
-import plex.camera as camera
+from plex.camera import Camera
+from plex.motor import Motor
+import plex.line_follow as line_follow
 import cv2
 
-# init
-cam = camera.Camera()
+# Robot nodes
+cam = Camera()
+
+# initializations
+line_follow.init(cam)
 
 while(True):
-    frame = cam.get_mask_frame(camera.WHITE)
-
+    frame = line_follow.get_line_mask()
     cv2.imshow('frame', frame)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
--- src/plex/camera.py
+++ src/plex/camera.py
@@ -8,24 +8,17 @@ _K = np.array([[258.834980287596, 0.0, 315.06271814581396], [0.0, 259.2571348729
 _D = np.array([[0.03771034946935066], [-0.06873597576438498], [0.19581964746039696], [-0.2649961510264901]])
 _MAP_1, _MAP_2 = cv2.fisheye.initUndistortRectifyMap(_K, _D, np.eye(3), _K, (WIDTH, HEIGHT), cv2.CV_16SC2)
 
-WHITE = (np.array([0,0,100]), np.array([255,35,255]))
-
 class Camera:
 	def __init__(self):
 		# private
 		self._cap = cv2.VideoCapture(0)
 		# public
 	
-	def _get_frame(self) -> np.ndarray:
+	def get_frame(self) -> np.ndarray:
 		ret, frame = self._cap.read()
 		assert ret, "There is an error while reading VideoCapture" # TODO: return blank frame if error occurred
 		frame = cv2.remap(frame, _MAP_1, _MAP_2, interpolation=cv2.INTER_LINEAR, borderMode=cv2.BORDER_CONSTANT) # fisheye cleaning
 		return frame
-	
-	def get_mask_frame(self, color: tuple) -> np.ndarray:
-		hsv_frame = cv2.cvtColor(self._get_frame(), cv2.COLOR_BGR2HSV)
-		mask = cv2.inRange(hsv_frame, color[0], color[1])
-		return mask
 	
 	def __del__(self):
 		self._cap.release()
