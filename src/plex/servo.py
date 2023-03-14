409345e3545064e72dbaee1d691879448e03e08e
SamalPunsara <81386128+SamalPunsara@users.noreply.github.com>
2023-03-14 17:42:29 +0530

    Update servo.py

--- src/plex/servo.py
+++ src/plex/servo.py
@@ -4,21 +4,24 @@ import time
 class Servo:
     def __init__(self, servoPin):
         self.servoPin = servoPin
-        GPIO.setmode(GPIO.BCM)
+        GPIO.setmode(GPIO.BCM) 
         GPIO.setup(self.servoPin, GPIO.OUT)
-        self.pwm = GPIO.PWM(self.servoPin, 50) # PWM signal at 50Hz
+        self.pwm = GPIO.PWM(self.servoPin, 50) # PWM signal at 50Hz also depends on the servo
         self.pwm.start(0)
 
-    def set_angle(self, angle):
+    def set_angle(self, angle): # angle is between 0 and 180, depends on the servo
         duty_cycle = 2 + (angle/18) # calculate duty cycle from angle
         self.pwm.ChangeDutyCycle(duty_cycle)
+        time.sleep(0.5)
+   
+    def reset(self):
+        self.set_angle(0)
         time.sleep(0.5)
 
     def cleanup(self):
         self.pwm.stop()
         GPIO.cleanup()
 
-    def reset(self):
-        self.set_angle(0)
+    
         
     
