8954caacc367bc9c0b21088b75e5315bbee70155
dakshinatharindu <tharinduokd@gmail.com>
2023-03-14 20:03:48 +0530

    fix motor pwm

--- src/main.py
+++ src/main.py
@@ -1,19 +1,18 @@
 from plex.camera import Camera
-from plex.motor import Motor
+import plex.motor as motor
 import plex.line_follow as line_follow
 import cv2
-
+from time import sleep
 # Robot nodes
-cam = Camera()
+# cam = Camera()
+left_motor = motor.Motor(ena=2, motor_out_A=18, motor_out_B=4, enc_in_A=17, enc_in_B=27)
 
 # initializations
-line_follow.init(cam)
+# line_follow.init(cam)
 
-while(True):
-    frame = line_follow.test()
-    cv2.imshow('frame', frame)
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
+
+left_motor.set_dir(motor.DIR_CLKWS)
+left_motor.set_speed(10)
+sleep(5)  
+left_motor.stop()
     
-
-cv2.destroyAllWindows()
