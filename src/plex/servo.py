ba2f472b00deaab31ab8baf9828d99e0e75f3bea
SamalPunsara <81386128+SamalPunsara@users.noreply.github.com>
2023-03-15 22:15:47 +0530

    Gradual Change of Angle

--- src/plex/servo.py
+++ src/plex/servo.py
@@ -2,20 +2,35 @@ import RPi.GPIO as GPIO
 import time
 
 class Servo:
-    def __init__(self, servoPin):
+    def __init__(self, servoPin, initialAngle = 0):
         self.servoPin = servoPin
         GPIO.setmode(GPIO.BCM) 
-        GPIO.setup(self.servoPin, GPIO.OUT)
-        self.pwm = GPIO.PWM(self.servoPin, 50) # PWM signal at 50Hz also depends on the servo
+        GPIO.setup(servoPin, GPIO.OUT)
+        self.pwm = GPIO.PWM(servoPin, 50) # PWM signal at 50Hz also depends on the servo
         self.pwm.start(0)
+        
+        self.currentAngle = initialAngle # to track the current angle of the servo
+        self.set_angle(initialAngle)
 
     def set_angle(self, angle): # angle is between 0 and 180, depends on the servo
-        duty_cycle = 2 + (angle/18) # calculate duty cycle from angle
-        self.pwm.ChangeDutyCycle(duty_cycle)
-        time.sleep(0.5)
+        if angle > self.currentAngle:
+            for i in range(self.currentAngle, angle + 1):
+                duty_cycle = 2 + (i/18) # calculate duty cycle from angle
+                self.pwm.ChangeDutyCycle(duty_cycle)
+                self.currentAngle = i
+                time.sleep(0.05) # 50 ms delay --- can change if necessary
+        elif angle < self.currentAngle:
+            for i in range(self.currentAngle, angle - 1, -1):
+                duty_cycle = 2 + (i/18)
+                self.pwm.ChangeDutyCycle(duty_cycle)
+                self.angle = i
+                time.sleep(0.05)
+        
+       
    
     def reset(self):
         self.set_angle(0)
+        self.currentAngle = 0
         time.sleep(0.5)
 
     def cleanup(self):
