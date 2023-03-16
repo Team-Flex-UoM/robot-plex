dca502546ddb4bcb309c796930c51b1325677efb
CSAapps <csasoftwares@gmail.com>
2023-03-16 23:24:30 +0530

    create camera class for webot

--- sim/cv-test/controllers/plex_controller/plex/camera.py
+++ sim/cv-test/controllers/plex_controller/plex/camera.py
@@ -1,5 +1,6 @@
 import cv2
 import numpy as np
+from controller import Robot,Camera,Display
 
 # Class constants
 WIDTH = 640
@@ -9,19 +10,38 @@ _D = np.array([[0.03926573368675932], [-0.012201178250275507], [-0.0083554123570
 _MAP_1, _MAP_2 = cv2.fisheye.initUndistortRectifyMap(_K, _D, np.eye(3), _K, (WIDTH, HEIGHT), cv2.CV_16SC2)
 
 class Camera:
-	def __init__(self):
+	def __init__(self,robot):
 		# private
-		self._cap = cv2.VideoCapture(0)
+		timestep = int(robot.getBasicTimeStep())
+		self._camera=robot.getDevice("camera")
+		self._camera.enable(timestep)
+		self.imageWidth=self._camera.getWidth()
+		self.imageHeight=self._camera.getHeight()
+		self._display=robot.getDevice("display")
+		
 		# public
 	
 	def get_frame(self) -> np.ndarray:
-		ret, frame = self._cap.read()
-		assert ret, "There is an error while reading VideoCapture" # TODO: return blank frame if error occurred
-		frame = cv2.remap(frame, _MAP_1, _MAP_2, interpolation=cv2.INTER_LINEAR, borderMode=cv2.BORDER_CONSTANT) # fisheye cleaning
-		return frame
+		image_in_raw=self._camera.getImage()  
+		image_in_list=list(image_in_raw)
+		image_in_arr=np.array(image_in_list,dtype=np.uint8)
+		image_in_bgra=np.reshape(image_in_arr,(self.imageHeight,self.imageWidth,4)) # image format : BGRA
+		# image=np.reshape(image_in_arr,(imageHeight,imageWidth,4)) [:,:,:-1]  # image format : BGR
+		image_in_bgr=cv2.cvtColor(image_in_bgra,cv2.COLOR_BGRA2BGR)
+		return image_in_bgr
+
+	def show_frame(self,frame):
+		image_out_bgr=cv2.cvtColor(frame,cv2.COLOR_BGR2BGRA)
+		image_out_arr=image_out_bgr.flatten()
+		image_out_list=list(image_out_arr)    
+		image_out_raw=bytes(image_out_list)
+		ir=self._display.imageNew(image_out_raw,Display.BGRA,self.imageWidth,self.imageHeight)
+		self._display.imagePaste(ir,0,0,False)
+		self._display.imageDelete(ir)
+    
 	
 	def __del__(self):
-		self._cap.release()
+		pass
 
 if __name__ == "__main__":
     print("Please run the 'main.py' file")
--- sim/cv-test/controllers/plex_controller/plex_controller.py
+++ sim/cv-test/controllers/plex_controller/plex_controller.py
@@ -2,15 +2,17 @@
 
 # You may need to import some classes of the controller module. Ex:
 #  from controller import Robot, Motor, DistanceSensor
-from controller import Robot,Camera,Display
+from controller import Robot
 # from plex.motor import Motor
 import plex.motor as motor
 from plex.motor import Motor
 
+import plex.camera as camera
+from plex.camera import Camera
 
 
 
-# import cv2
+import cv2
 import numpy as np
 
 
@@ -21,51 +23,27 @@ motorLeft.set_dir(motor.DIR_CLKWS)
 motorLeft.set_speed(10)
 motorLeft.start()
 
+cam=Camera(robot)
+
 print("hello")
-
-
 
 # # get the time step of the current world.
 timestep = int(robot.getBasicTimeStep())
 
 
-# camera=robot.getDevice("camera")
-# camera.enable(timestep)
-# imageWidth=camera.getWidth()
-# imageHeight=camera.getHeight()
-# display=robot.getDevice("display")
+# Main loop:
+# - perform simulation steps until Webots is stopping the controller
+while robot.step(timestep) != -1:
 
-# # Main loop:
-# # - perform simulation steps until Webots is stopping the controller
-# while robot.step(timestep) != -1:
-
-#     # # get image
+    # # get image
     
-#     image_in_raw=camera.getImage()  
-#     image_in_list=list(image_in_raw)
-#     image_in_arr=np.array(image_in_list,dtype=np.uint8)
-#     image_in_bgra=np.reshape(image_in_arr,(imageHeight,imageWidth,4)) # image format : BGRA
-#     # image=np.reshape(image_in_arr,(imageHeight,imageWidth,4)) [:,:,:-1]  # image format : BGR
-#     image_in_bgr=cv2.cvtColor(image_in_bgra,cv2.COLOR_BGRA2BGR)
-#     # # process image
+    image_in_bgr=cam.get_frame()
 
-#     image_edit=cv2.circle(image_in_bgr,(imageWidth//2,imageHeight//2),min(imageHeight,imageWidth)//4,(26,181,0),2)
+    image_edit=cv2.circle(image_in_bgr,(cam.imageWidth//2,cam.imageHeight//2),min(cam.imageHeight,cam.imageWidth)//4,(26,181,0),2)
 
-#     # image_bw=cv2.cvtColor(image_edit,cv2.COLOR_BGRA2GRAY)
-
-#     # image_edit=cv2
-
-#     # # display image
-
-#     image_out_bgr=cv2.cvtColor(image_edit,cv2.COLOR_BGR2BGRA)
-#     image_out_arr=image_out_bgr.flatten()
-#     image_out_list=list(image_out_arr)    
-#     image_out_raw=bytes(image_out_list)
-#     ir=display.imageNew(image_out_raw,Display.BGRA,imageWidth,imageHeight)
-#     display.imagePaste(ir,0,0,False)
-#     display.imageDelete(ir)
+    cam.show_frame(image_edit)
     
-#     pass
+    pass
 
 # Enter here exit cleanup code.
 
