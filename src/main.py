1aa5657841d56189a16116c81f5e4e334acefd90
dakshinatharindu <tharinduokd@gmail.com>
2023-03-18 16:59:11 +0530

    merge to main

--- src/main.py
+++ src/main.py
@@ -1,16 +1,23 @@
-from plex.camera import Camera
+import plex.camera as camera
 import plex.motor as motor
 import plex.line_follow as line_follow
 import cv2
 from time import sleep
+
 # Robot nodes
+cam = camera.Camera()
 
 # initializations
-# line_follow.init(cam)
+line_follow.init(cam)
 
-
-left_motor.set_dir(motor.DIR_CLKWS)
-left_motor.set_speed(10)
-sleep(5)  
-left_motor.stop()
+while(True):
+    frame = line_follow.test()
+    cv2.imshow('frame', frame)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
     
+
+cv2.destroyAllWindows()
+# line_follow.test()
+    
+
