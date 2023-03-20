e276cc072339c0a87418877de0a2077073a8d6e1
dakshinatharindu <tharinduokd@gmail.com>
2023-03-20 17:27:47 +0530

    go_distance func

--- src/plex/motor_driver.py
+++ src/plex/motor_driver.py
@@ -1,6 +1,8 @@
 import plex.motor as motor
 from gpiozero import RotaryEncoder
 
+LEFT = 0
+RIGHT = 1
 TURN_SPEED = 15
 TURN_ENC_COUNT = 100
 
@@ -23,9 +25,32 @@ def forward(left_motor_speed, right_motor_speed) -> None:
     left_motor.start()
     right_motor.start()
 
-def turn_left() -> None:
-    left_motor.set_dir(motor.DIR_CLKWS)
-    right_motor.set_dir(motor.DIR_CLKWS)
+def turn(dir: int) -> None:
+    if dir == LEFT:
+        left_motor.set_dir(motor.DIR_ANTCLKWS)
+        right_motor.set_dir(motor.DIR_CLKWS)
+    else:
+        left_motor.set_dir(motor.DIR_ANTCLKWS)
+        right_motor.set_dir(motor.DIR_CLKWS)
+    
+    left_motor.set_speed(TURN_SPEED)
+    right_motor.set_speed(TURN_SPEED)
+    right_encoder.steps = 0
+    left_motor.start()
+    right_motor.start()
+    while right_encoder.steps <= TURN_ENC_COUNT:
+        continue
+    left_motor.stop()
+    right_motor.stop()
+
+def go_distance(dis: int):
+    if dir > 0:
+        left_motor.set_dir(motor.DIR_ANTCLKWS)
+        right_motor.set_dir(motor.DIR_ANTCLKWS)
+    else:
+        left_motor.set_dir(motor.DIR_CLKWS)
+        right_motor.set_dir(motor.DIR_CLKWS)
+    
     left_motor.set_speed(TURN_SPEED)
     right_motor.set_speed(TURN_SPEED)
     right_encoder.steps = 0
