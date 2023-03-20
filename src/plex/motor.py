339d7e931bd9a7f366498a29ec96eee3f5149869
dakshinatharindu <tharinduokd@gmail.com>
2023-03-20 14:20:00 +0530

    added encoder methods

--- src/plex/motor.py
+++ src/plex/motor.py
@@ -1,7 +1,10 @@
 import RPi.GPIO as GPIO
+
 DIR_CLKWS=0
 DIR_ANTCLKWS=1
 FREQUENCY=50 #50Hz
+MAX_SPEED = 80
+MIN_SPEED = 10
 
 class Motor:    
     def __init__(self, ena, motor_out_A, motor_out_B) -> None:
@@ -20,18 +23,19 @@ class Motor:
         GPIO.output(self.motor_out_B, dir)
 
     def set_speed(self,speed) -> None:
-        self.pwm.start(speed)
+        self.pwm.start(max(min(speed, MAX_SPEED), MIN_SPEED))
 
     def brake(self) -> None:
         GPIO.output(self.motor_out_A, 1)
         GPIO.output(self.motor_out_B, 1)
 
     def stop(self) -> None:
-        GPIO.output(self.ena,0)
+        GPIO.output(self.ena, 0)
 
     def start(self) -> None:
-        GPIO.output(self.ena,1)
+        GPIO.output(self.ena, 1)
     
-    def __del__(self):
+    def __del__(self) -> None:
+        GPIO.setmode(GPIO.BCM)
         GPIO.cleanup([self.ena, self.motor_out_A, self.motor_out_B])
         
