bc1eef8c6efeff101629631a66520dc39fc478f2
CSA <abeywickramakcs.19@uom.lk>
2023-03-21 20:47:55 +0530

    implement encorder class

--- src/plex/motor.py
+++ src/plex/motor.py
@@ -1,13 +1,20 @@
 import RPi.GPIO as GPIO
+import plex.encoder as encoder
 
 DIR_CLKWS=0
 DIR_ANTCLKWS=1
 FREQUENCY=50 #50Hz
 MAX_SPEED = 80
 MIN_SPEED = 10
+KP = 0.5
+KI = 0
+KD = 0
+
+prev_error = 0
+acc_error = 0
 
 class Motor:    
-    def __init__(self, ena, motor_out_A, motor_out_B) -> None:
+    def __init__(self, ena, motor_out_A, motor_out_B, encoder_node: encoder.Encoder) -> None:
         GPIO.setmode(GPIO.BCM)
         GPIO.setup(ena, GPIO.OUT)
         GPIO.setup(motor_out_A, GPIO.OUT)
@@ -16,18 +23,27 @@ class Motor:
         self.ena=ena
         self.motor_out_A=motor_out_A
         self.motor_out_B=motor_out_B
+        self.encoder = encoder_node
 
         self.pwm = GPIO.PWM(motor_out_A, FREQUENCY)
+        self.pwm.start(0)      
+    
+    def pid(self, ref_speed):
+        global prev_error
+        global acc_error
+
+        error = ref_speed - self.encoder.speed
+        out_speed = KP*error + KI*(acc_error + error) + KD*(error - prev_error)
+        acc_error += error
+        prev_error = error
+        print(ref_speed, self.encoder.speed)
+        return out_speed
 
     def set_dir(self, dir) -> None:
         GPIO.output(self.motor_out_B, dir)
 
     def set_speed(self,speed) -> None:
-        self.pwm.start(max(min(speed, MAX_SPEED), MIN_SPEED))
-
-    def brake(self) -> None:
-        GPIO.output(self.motor_out_A, 1)
-        GPIO.output(self.motor_out_B, 1)
+        self.pwm.ChangeDutyCycle(max(min(speed, MAX_SPEED), MIN_SPEED))
 
     def stop(self) -> None:
         GPIO.output(self.ena, 0)
--- src/test.py
+++ src/test.py
@@ -1,23 +1,59 @@
-import plex.line_follow as line_follow
-import plex.camera as camera
-from plex.camera import Camera
-import cv2
+# import plex.line_follow as line_follow
+# import plex.camera as camera
+# from plex.camera import Camera
+# import cv2
 
-cam = Camera()
-# initializations
-line_follow.init(cam)
-while True:
-    # roi,img=line_follow.get_roi()
-    # frame=line_follow.process_roi(roi)
-    # cv2.circle(roi,(2,2),3,(0,255,0),3)
-    # frame=cam.get_,frame()
+# cam = Camera()
+# # initializations
+# line_follow.init(cam)
+# while True:
+#     # roi,img=line_follow.get_roi()
+#     # frame=line_follow.process_roi(roi)
+#     # cv2.circle(roi,(2,2),3,(0,255,0),3)
+#     # frame=cam.get_,frame()
    
-    # cv2.imshow('img',img)
-    # cv2.imshow('roi',roi)
-    # cv2.imshow('frame',frame)
+#     # cv2.imshow('img',img)
+#     # cv2.imshow('roi',roi)
+#     # cv2.imshow('frame',frame)
 
-    line_follow.follow()
+#     line_follow.follow()
 
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
-cv2.destroyAllWindows()
+#     if cv2.waitKey(1) & 0xFF == ord('q'):
+#         break
+# cv2.destroyAllWindows()
+
+
+import plex.motor as motor
+from plex.encoder import Encoder
+import plex.motor_driver as motor_driver
+from time import sleep,time
+en = Encoder(20, 21)
+en1 = Encoder(4,14)
+left_motor = motor.Motor(26, 12, 16, en)
+right_motor = motor.Motor(2, 18, 3, en1)
+# right_motor = Motor(2, 18, 3)
+
+# motor_driver.init(left_motor, right_motor)
+
+left_motor.set_dir(motor.DIR_ANTCLKWS)
+right_motor.set_dir(motor.DIR_CLKWS)
+
+# left_motor.set_dir(motor.DIR_CLKWS)
+# right_motor.set_dir(motor.DIR_ANTCLKWS)
+
+left_motor.set_speed(50)
+left_motor.start()
+right_motor.set_speed(50)
+right_motor.start()
+sleep(0.3)
+left_motor.set_speed(30)
+right_motor.set_speed(30)
+
+while True:
+    # left_motor.set_speed(50)
+    # left_motor.start()
+    # right_motor.set_speed(50)
+    # right_motor.start()
+    pass
+
+
